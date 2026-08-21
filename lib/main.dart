8ce25d2109c30cc055eb449ce6d8f2e288febb95
NishimuraShigenori <nishimura@mon.co.jp>
2026-08-21 11:28:27 +0900

    remove unused test file

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MemoPageState extends State<MemoPage> {
       final Uint8List imageBytes = await image.readAsBytes();
       final String base64Body = base64Encode(imageBytes);
 
-      final Uri url = Uri.parse('https://imgbb.com');
+      final Uri url = Uri.parse('https://api.imgbb.com/1/upload');
       
       final response = await http.post(
         url,
